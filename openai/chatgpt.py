4dbe0d45ad3b051d295e56024c6f09a57d05979d
Roman Pavelka <roman.pavelka@siemens.com>
2023-08-01 15:34:36 +0200

    Add GPT-4 support

--- openai/chatgpt.py
+++ openai/chatgpt.py
@@ -3,7 +3,7 @@ import openai
 import os
 import sys
 
-MODEL = "text-davinci-003"
+MODEL = "gpt-4"
 
 os.chdir(os.path.dirname(__file__))
 
@@ -12,7 +12,17 @@ with open('.api_key', 'r') as f:
 
 prompt = " ".join(sys.argv[1:])
 
-response = openai.Completion.create(
-    model=MODEL, prompt=prompt, temperature=0.1, max_tokens=4000)
+# System message can provide further control over tone and task
+# There is a way how to send more advanced discussion too
+messages = [
+#        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt},
+#        {"role": "user", "content": "Knock knock."},
+#        {"role": "assistant", "content": "Who's there?"},
+#        {"role": "user", "content": "Orange."},  # And model would proceed with "Orange who?"
+]
 
-print(response.choices[0].text.strip())
+response = openai.ChatCompletion.create(
+    model=MODEL, messages=messages, temperature=0.1)
+
+print(response.choices[0]["message"]["content"].strip())
